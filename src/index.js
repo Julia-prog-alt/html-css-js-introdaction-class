1012de9754fbe9f19ec8d7c7b70d818583ded9bf
JuliaBiriukova <julia09071993@gmail.com>
2022-10-09 20:53:27 +0300

    HW#8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 const anchors = document.querySelectorAll(".thubnails-anchor");
 const detailsImage = document.querySelector("details-image");
 const detailsTitle = document.querySelector("details-title");
+function setDetails (anchor) {
+    detailsImage.setAttribute('src',anchor.getAttribute('data-details-image'));
+    detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
+}
+for(let i = 0; < anchors.length; i++) {
+    anchors[i].addEventListener("click",function(){
+        setDetails(anchors[i]);
+    })
+}
