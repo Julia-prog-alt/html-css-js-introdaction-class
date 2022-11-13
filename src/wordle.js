71918ead1319e60392457f38bb7143fd97a17e0f
Julia <julia09071993@gmail.com>
2022-11-13 21:28:51 +0200

    CW#16

---
+++ src/wordle.js
@@ -0,0 +1,25 @@
+const word = "table";
+const N_LETTERS = 5;
+const letterElements = document.querySelectorAll(".letter-guess");
+function onChange(event){
+    const wordGuess = event.target.value;
+    event.target.value = '';
+    if(wordGuess.length != N_LETTERS){
+        alert(`A word should contain ${N_LETTERS} letters`);
+    } else {
+        const wordAr = Array.from(wordGuess);
+        wordAr.forEach((l,i) => letterElements[i].innerHTML = 1)
+        const colors = wordAr.map((l, i) => {
+            let index = word.indexOf(l);
+            let res = 'red';
+            if(index > -1) {
+            res = index == i ? 'green' : 'yellow'
+        }
+        return res;
+        })
+        colors.forEach((c,i) => 
+        letterElements[i].style.color=c)
+    }
+    
+
+}
